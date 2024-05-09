3df6a944903f88786de15dec7ff50c28b9e3d6a8
Daniel Moreno <danmore@nmsu.edu>
2024-05-09 16:08:30 -0600

    Initial Commit

---
+++ Parcel/ParcelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParcelApp.swift
+//  Parcel
+//
+//  Created by Daniel Moreno on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ParcelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
