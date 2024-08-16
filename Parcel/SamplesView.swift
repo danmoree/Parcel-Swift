28f36ab2494b698f5561b6a00e4c6fbdb2fc8c51
Daniel Moreno <danmore@nmsu.edu>
2024-08-16 15:49:43 -0600

    Fully implemented

---
+++ Parcel/SamplesView.swift
@@ -0,0 +1,18 @@
+//
+//  SamplesView.swift
+//  Parcel
+//
+//  Created by Daniel Moreno on 8/16/24.
+//
+
+import SwiftUI
+
+struct SamplesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SamplesView()
+}
